5503b8e3f9448ac3a5b9b6e5e7d490216a8f12c8
freewind <nowindlee@gmail.com>
2019-01-23 16:46:19 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-TypeScript Puppeteer Demo
-=======================================
+TypeScript Puppeteer Check For DOM Changed by AJAX Demo
+=======================================================
+
+In single page application or updating DOM with ajax request, in order to make sure the operation is finished,
+the correct way to do is to check and wait expected DOM directly, and the wrong way is to
+use `page.waitForNavigation` since there is no navigation in this case.
 
 ```
 npm install -g puppeteer
+npm run server
 npm run demo
 ```
 
--- hello.ts
+++ hello.ts
@@ -1,11 +1,76 @@
 import * as puppeteer from 'puppeteer';
+import {Page} from "puppeteer";
+
+async function correctWayToClickAndCheck(page: Page) {
+  console.log('---------- correctWayToClickAndCheck ---------')
+  await page.goto("http://localhost:8989/index.html");
+
+  await page.click('#button');
+  await page.waitForFunction(() => {
+    const data = document.querySelector('#data');
+    return data && data.innerHTML.length > 0;
+  })
+}
+
+async function wrongWayToUseNetworkIdle2(page: Page) {
+  console.log('---------- wrongWayToUseNetworkIdle2 --------------');
+  await page.goto("http://localhost:8989/index.html");
+
+  await Promise.all([
+    page.click('#button'),
+    page.waitForNavigation({waitUntil: 'networkidle2'}),
+  ])
+}
+
+async function wrongWayToUseDomContentLoaded(page: Page) {
+  console.log('---------- wrongWayToUseDomContentLoaded --------------');
+  await page.goto("http://localhost:8989/index.html");
+
+  await Promise.all([
+    page.click('#button'),
+    page.waitForNavigation({waitUntil: 'domcontentloaded'}),
+  ])
+}
+
+async function wrongWayToUseLoad(page: Page) {
+  console.log('---------- wrongWayToUseLoad --------------');
+  await page.goto("http://localhost:8989/index.html");
+
+  await Promise.all([
+    page.click('#button'),
+    page.waitForNavigation({waitUntil: 'load'}),
+  ])
+}
+
 
 async function run() {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  console.log('open github');
-  await page.goto("http://github.com");
+  await page.setDefaultNavigationTimeout(2000);
+
+  await correctWayToClickAndCheck(page);
+
+  try {
+    await wrongWayToUseDomContentLoaded(page);
+  } catch (e) {
+    console.log('Expect error thrown:')
+    console.error(e);
+  }
+
+  try {
+    await wrongWayToUseNetworkIdle2(page);
+  } catch (e) {
+    console.log('Expect error thrown:')
+    console.error(e);
+  }
+
+  try {
+    await wrongWayToUseLoad(page);
+  } catch (e) {
+    console.log('Expect error thrown:')
+    console.error(e);
+  }
 
   await browser.close();
 }
